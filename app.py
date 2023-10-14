6738471409e60978bdb5cc0558185fceea157068
Anmol6Srivastava9 <143920637+Anmol6Srivastava9@users.noreply.github.com>
2023-10-15 04:02:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def receive_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0")
